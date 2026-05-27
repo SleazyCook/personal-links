2b4404644296da5797286f026308bc3424a76bda
Andrew Cook <andrewjcook93@gmail.com>
2026-05-26 22:18:07 -0500

    update links for github, letterboxd

--- src/data/links.js
+++ src/data/links.js
@@ -18,7 +18,7 @@ const links = [
   {
     id: 3,
     label: "TikTok",
-    href: "https://www.tiktok.com/@sleazycook",
+    href: "https://www.tiktok.com/@developedbydrewford",
     img: "https://cdn.simpleicons.org/tiktok/ffffff",
     alt: "TikTok logo",
     color: "#010101",
@@ -26,7 +26,7 @@ const links = [
   {
     id: 4,
     label: "Letterboxd",
-    href: "https://letterboxd.com/sleazycook/",
+    href: "https://letterboxd.com/DevByDrewford/",
     img: "https://cdn.simpleicons.org/letterboxd/ffffff",
     alt: "Letterboxd logo",
     color: "#2c3440",
@@ -34,7 +34,7 @@ const links = [
   {
     id: 5,
     label: "GitHub",
-    href: "https://github.com/SleazyCook",
+    href: "https://github.com/DevelopedByDrewford",
     img: "https://cdn.simpleicons.org/github/ffffff",
     alt: "GitHub logo",
     color: "#24292e",
